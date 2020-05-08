5946ae670fc4aa91d671faa437ddec4e000ae50d
Chase Johnson <Chase.b.johnson@gmail.com>
2020-05-07 18:53:37 -0700

    Cleaned up JS. Still working on clear all button

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,27 @@
 
-
+// Openweather API Key //
 const apiKey = "6c0ac38b22e3e819b50460a5a899f855";
+// Current Weather Info //
 var currWeatherDiv = $("#currentWeather");
+// Five Day Forcast Info //
 var forecastDiv = $("#weatherForecast");
+// City Array //
 var citiesArray;
 
+// City search when user clicks search icon
+$("#submitCity").click(function() {
+    event.preventDefault();
+    let cityName = $("#cityInput").val();
+    returnCurrentWeather(cityName);
+    returnWeatherForecast(cityName);
+});
+
+// Previous Citys show under search 
+$("#previousSearch").click(function() {
+    let cityName = event.target.value;
+    returnCurrentWeather(cityName);
+    returnWeatherForecast(cityName);
+})
 
 // Local Storage functionality // 
 if (localStorage.getItem("localWeatherSearches")) {
@@ -14,29 +31,31 @@ if (localStorage.getItem("localWeatherSearches")) {
     citiesArray = [];
 };
 
-//button to clear local storage and refresh the page
-
 // Call API for current weather by city name 
 function returnCurrentWeather(cityName) {
-    let queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&APPID=${apiKey}`;
+    let queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=imperial&APPID=${apiKey}`;
 
+    // Get info from URL When searched
     $.get(queryURL).then(function(response){
+        // Current Date 
         let currTime = new Date(response.dt*1000);
+        // Displays Weather Icon 
         let weatherIcon = `https://openweathermap.org/img/wn/${response.weather[0].icon}@2x.png`;
 
-        // 
+        // When searched display info on Weather info
         currWeatherDiv.html(`
         <h2>${response.name}, ${response.sys.country} (${currTime.getMonth()+1}/${currTime.getDate()}/${currTime.getFullYear()})<img src=${weatherIcon} height="70px"></h2>
-        <p>Temperature: ${response.main.temp} &#176;C</p>
+        <p>Temperature: ${response.main.temp}&#176;F</p>
         <p>Humidity: ${response.main.humidity}%</p>
-        <p>Wind Speed: ${response.wind.speed} m/s</p>
+        <p>Wind Speed: ${response.wind.speed} mph</p>
         `, returnUVIndex(response.coord))
         createHistoryButton(response.name);
     })
 };
 
+// Call API for weather forcast 
 function returnWeatherForecast(cityName) {
-    let queryURL = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&units=metric&APPID=${apiKey}`;
+    let queryURL = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&units=imperial&APPID=6c0ac38b22e3e819b50460a5a899f855`;
 
     $.get(queryURL).then(function(response){
         let forecastInfo = response.list;
@@ -45,16 +64,18 @@ function returnWeatherForecast(cityName) {
             if (!forecastInfo[i].dt_txt.includes("12:00:00")) {
                 return;
             }
+            // Forcast dates 
             let forecastDate = new Date(forecastInfo[i].dt*1000);
+            // Displays Weather Icon 
             let weatherIcon = `https://openweathermap.org/img/wn/${forecastInfo[i].weather[0].icon}.png`;
-
+            // Append data to 5 day forcast when searched 
             forecastDiv.append(`
             <div class="col-md">
                 <div class="card text-white bg-primary">
                     <div class="card-body">
                         <h4>${forecastDate.getMonth()+1}/${forecastDate.getDate()}/${forecastDate.getFullYear()}</h4>
                         <img src=${weatherIcon} alt="Icon">
-                        <p>Temp: ${forecastInfo[i].main.temp} &#176;C</p>
+                        <p>Temp: ${forecastInfo[i].main.temp}&#176;F</p>
                         <p>Humidity: ${forecastInfo[i].main.humidity}%</p>
                     </div>
                 </div>
@@ -65,7 +86,6 @@ function returnWeatherForecast(cityName) {
 };
 
 // The current UV index is collected at the same time as the current weather
-// by making use of the searched city's returned coordinates
 function returnUVIndex(coordinates) {
     let queryURL = `https://api.openweathermap.org/data/2.5/uvi?lat=${coordinates.lat}&lon=${coordinates.lon}&APPID=${apiKey}`;
 
@@ -91,8 +111,8 @@ function returnUVIndex(coordinates) {
     })
 }
 
+// Creats history of recent searches 
 function createHistoryButton(cityName) {
-    // Check if the button exists in history, and if it does, exit the function
     var citySearch = cityName.trim();
     var buttonCheck = $(`#previousSearch > BUTTON[value='${citySearch}']`);
     if (buttonCheck.length == 1) {
@@ -114,21 +134,3 @@ function writeSearchHistory(array) {
         createHistoryButton(array[i]);
     })
 }
-
-// Displaying Sacramento Weather 
-// This is when the page is loaded
-returnCurrentWeather("Sacramento");
-returnWeatherForecast("Sacramento");
-
-$("#submitCity").click(function() {
-    event.preventDefault();
-    let cityName = $("#cityInput").val();
-    returnCurrentWeather(cityName);
-    returnWeatherForecast(cityName);
-});
-
-$("#previousSearch").click(function() {
-    let cityName = event.target.value;
-    returnCurrentWeather(cityName);
-    returnWeatherForecast(cityName);
-})
